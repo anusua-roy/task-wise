a9fd79d8952fdcb3c3a7aaf8a8e6ab6df8f7cce7
anusua-roy <anusua.roy05@gmail.com>
2026-04-17 12:18:47 +0530

    build fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,28 +1,20 @@
 from fastapi import FastAPI
-from app.db.base import Base
-from app.db.session import engine
-from app.api import users, roles, projects, tasks
-from app.api import auth
+from app.api import users, roles, projects, tasks, auth
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="FSE Task Tracker")
 
-Base.metadata.create_all(bind=engine)
-
-# for CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # later restrict
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
 @app.get("/health")
 def health():
     return {"status": "Backend App is up and running successfully..."}
-
 
 app.include_router(auth.router)
 app.include_router(users.router)
